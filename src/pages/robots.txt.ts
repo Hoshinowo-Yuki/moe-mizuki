cb5a45c67afab8977b3e32859a7e5bf610babc20
星野有希 <hoshinoyuki@lolicon.io>
2026-07-24 02:07:12 +0800

    feat: allow facebookexternalhit inside robots.txt for crawling

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,24 +1,13 @@
 import type { APIRoute } from "astro";
 
 const robotsTxt = `
+User-agent: facebookexternalhit
+Allow: /
+
 User-agent: *
 Disallow: /
 Allow: /$
 Allow: /posts/
-
-Allow: /archive/
-Allow: /anime/
-Allow: /diary/
-Allow: /albums/
-Allow: /devices/
-Allow: /about/
-Allow: /friends/
-Allow: /updates/
-Allow: /license/
-Allow: /projects/
-Allow: /skills/
-Allow: /ai-tools/
-Allow: /timeline/
 
 Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
