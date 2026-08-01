e157399f5455bcfc52503f16df214acaf2826fe4
星野ゆき <hoshinoyuki@lolicon.io>
2026-08-01 11:32:38 +0800

    chore: attempting to fix robots.txt.ts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -9,20 +9,6 @@ Disallow: /
 Allow: /$
 Allow: /posts/
 
-Allow: /archive/
-Allow: /anime/
-Allow: /diary/
-Allow: /albums/
-Allow: /devices/
-Allow: /about/
-Allow: /friends/
-Allow: /updates/
-Allow: /license/
-Allow: /projects/
-Allow: /skills/
-Allow: /ai-tools/
-Allow: /timeline/
-
 Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
 
